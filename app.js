378ac8dfb7f4176561b56fa8478e050cfdbc4bd9
Pål Hamel <pal.hamel@gmail.com>
2021-03-16 14:45:41 +0100

    add error handling via try catch

--- app.js
+++ app.js
@@ -8,39 +8,57 @@ var Airtable = require('airtable')
 var base = new Airtable({ apiKey: apiAirTable }).base(baseAirTable)
 const table = base('uscreen')
 
+/*
+error handling via async try/catch (err)
+*/
+
+// function to console.log only the important parts in the return:
+const selectedInfo = (data) => {
+  // <<<<<<<<<<<
+  return {
+    id: data.id,
+    fields: data.fields,
+    // createdTime: data.createdTime, // <= not working
+  }
+}
+
 // ----------------------------------------------------
 
-// ==> get all records in table:
+// ==> GET all records in table
 const getRecords = async () => {
-  const records = await table
-    .select({ maxRecords: 3, view: 'Grid view' })
-    .firstPage()
-  console.log(records)
+  try {
+    const records = await table
+      .select({ maxRecords: 3, view: 'Grid view' })
+      .firstPage()
+    console.log('list all', records)
+  } catch (err) {
+    console.error(err)
+  }
 }
 // getRecords()
 
 // ----------------------------------------------------
 
-// ==> get record by id:
+// ==> GET record by id
 const getRecordById = async (theId) => {
-  const record = await table.find(theId)
-  console.log(record)
+  try {
+    const record = await table.find(theId)
+    console.log('list one:', record)
+  } catch (err) {
+    console.error(err)
+  }
 }
 // getRecordById('recg2D5xnT347MMdT')
 
 // ----------------------------------------------------
 
-// ==> create records
+// ==> CREATE records
 const createRecord = async (fields) => {
-  const createdRecord = await table.create(fields)
-  console.log(selectedInfo(createdRecord))
-}
-// function to clg only the important parts in return:
-const selectedInfo = (data) => { // <<<<<<<<<<<
-  return {
-    id: data.id,
-    fields: data.fields,
-    // createdTime: data.createdTime, // <= not working yet
+  try {
+    const createdRecord = await table.create(fields)
+    console.log('created:', selectedInfo(createdRecord))
+  } catch (err) {
+    console.error(err)
   }
 }
 // createRecord({
@@ -52,21 +70,28 @@ const selectedInfo = (data) => { // <<<<<<<<<<<
 // })
 // ----------------------------------------------------
 
-// ==> update records
- const updateRecord = async (id, fields) => {
-   const updatedRecord = await table.update(id, fields)
-   console.log(selectedInfo(updatedRecord))
- }
-
+// ==> UPDATE records
+const updateRecord = async (id, fields) => {
+  try {
+    const updatedRecord = await table.update(id, fields)
+    console.log('updated:', selectedInfo(updatedRecord))
+  } catch (err) {
+    console.error(err)
+  }
+}
 // updateRecord('rec4T7LlvkUMh6hiK', {
 //   name: "Benke von updated",
 // })
+// ----------------------------------------------------
 
-// ==> delete records
-
+// ==> DELETE records
 const deleteRecord = async (id) => {
-  const deletedRecords = await table.destroy(id)
-  console.log('deleted:', selectedInfo(deletedRecords))
+  try {
+    const deletedRecords = await table.destroy(id)
+    console.log('deleted:', selectedInfo(deletedRecords))
+  } catch (err) {
+    console.error(err)
+  }
 }
 
 deleteRecord('recUuA1wXiyyc1rvg')
