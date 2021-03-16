c732c495b962e0a0e39e0b7e95c7a82a2c8fce8f
Pål Hamel <pal.hamel@gmail.com>
2021-03-16 12:32:47 +0100

    add create record

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 require('dotenv').config()
-// console.log(process.env.API_KEY_AIRTABLE)
-// console.log(process.env.BASE_AIRTABLE)
-var Airtable = require('airtable')
-var base = new Airtable({ apiKey: process.env.API_KEY_AIRTABLE }).base(
-  process.env.BASE_AIRTABLE
-)
-// console.log(base)
-const table = base('uscreen')
-// console.log(table)
 
-// ==> get all:
+const apiAirTable = process.env.API_KEY_AIRTABLE
+const baseAirTable = process.env.BASE_AIRTABLE
+
+var Airtable = require('airtable')
+
+var base = new Airtable({ apiKey: apiAirTable }).base(baseAirTable)
+const table = base('uscreen')
+
+// ==> get all records in table:
 const getRecords = async () => {
   const records = await table
     .select({ maxRecords: 3, view: 'Grid view' })
@@ -18,9 +17,23 @@ const getRecords = async () => {
 }
 // getRecords()
 
-// ==> get by id:
+// ==> get record by id:
 const getRecordById = async (theId) => {
   const record = await table.find(theId)
   console.log(record)
 }
-getRecordById('recg2D5xnT347MMdT')
+// getRecordById('recg2D5xnT347MMdT')
+
+// ==> create records
+const createRecord = async (fields) => {
+  const createdRecords = await table.create(fields)
+  console.log(createdRecords)
+}
+
+createRecord({
+  id: 200,
+  name: 'Jim',
+  email: 'jim@email.se',
+  custom_fields: 'new addition',
+  event: 'user_created',
+})
